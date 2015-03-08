1fd464da627060628911a6cdb2df6d840fcfb124
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-03-08 15:20:35 -0400

    Suppress logging in tests.

--- tests/test_conduction.py
+++ tests/test_conduction.py
@@ -3,6 +3,8 @@
 
 """Test Mongo Conduction."""
 
+import logging
+
 import pymongo
 from mockupdb import go
 from pymongo.errors import OperationFailure
@@ -15,6 +17,8 @@ class ConductionTest(unittest.TestCase):
     def setUp(self):
         self.mockup = get_mockup(releases={}, env=None,
                                  port=None, verbose=False)
+        # Quiet.
+        logging.getLogger('mongo_orchestration.apps').setLevel(logging.CRITICAL)
         self.mockup.run()
         self.loop_future = go(main_loop, self.mockup)
 
