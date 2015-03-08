ea6f15c469f79ebef83c02b8df3ff2bf825e60cd
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-03-08 08:52:26 -0400

    Start testing.

--- tests/test_conduction.py
+++ tests/test_conduction.py
@@ -3,7 +3,31 @@
 
 """Test Mongo Conduction."""
 
+import pymongo
+from mockupdb import go
+from pymongo.errors import OperationFailure
+
+from conduction.server import get_mockup, main_loop
 from tests import unittest  # unittest2 on Python 2.6.
+
+
+class ConductionTest(unittest.TestCase):
+    def setUp(self):
+        self.mockup = get_mockup(releases={}, env=None,
+                                 port=None, verbose=False)
+        self.mockup.run()
+        self.loop_future = go(main_loop, self.mockup)
+
+        # Cleanups are LIFO: Stop the server, wait for the loop to exit.
+        self.addCleanup(self.loop_future)
+        self.addCleanup(self.mockup.stop)
+
+        self.conduction = pymongo.MongoClient(self.mockup.uri).test
+
+    def test_bad_command_name(self):
+        with self.assertRaises(OperationFailure):
+            self.conduction.command('foo')
+
 
 if __name__ == '__main__':
     unittest.main()
