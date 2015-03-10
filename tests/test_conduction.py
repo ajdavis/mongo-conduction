503cdf57cc5cde0108f2e443cae3b3c4e6f9d789
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-03-10 03:54:34 -0400

    Quiet *all* logs in tests.

--- tests/test_conduction.py
+++ tests/test_conduction.py
@@ -18,7 +18,7 @@ class ConductionTest(unittest.TestCase):
         self.mockup = get_mockup(releases={}, env=None,
                                  port=None, verbose=False)
         # Quiet.
-        logging.getLogger('mongo_orchestration.apps').setLevel(logging.CRITICAL)
+        logging.getLogger('mongo_orchestration').setLevel(logging.CRITICAL)
         self.mockup.run()
         self.loop_future = go(main_loop, self.mockup)
 
